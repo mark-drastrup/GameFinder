e10683d71ebe858416b0544cf640345fd8d71c57
mark-drastrup <mark.drastrup@gmail.com>
2019-12-23 10:09:13 +0100

    generating list of latest releases

---
+++ src/components/LatestRelease.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="game">
+    <img :src="game.background_image" :alt="game.name" class="game__img" />
+    <p>{{game.name}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LatestRelease",
+  props: {
+    game: Object
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.game__img {
+  width: 300px;
+  height: auto;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,19 @@
 <template>
   <main class="home">
     <h1>Latest Releases</h1>
+    <div class="home__releases">
+      <latest-release v-for="game in latestReleases" :key="game.id" :game="game" />
+    </div>
   </main>
 </template>
 
 <script>
+import latestRelease from "@/components/LatestRelease.vue";
 export default {
   name: "home",
+  components: {
+    latestRelease
+  },
   computed: {
     latestReleases() {
       return this.$store.state.latestReleases;
@@ -18,5 +25,10 @@ export default {
 <style lang="scss" scoped>
 .home {
   padding: 15px;
+}
+
+.home__releases {
+  display: flex;
+  flex-wrap: wrap;
 }
 </style>
